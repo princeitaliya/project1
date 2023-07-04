6b4c87c6b4ddda8b16554525a79ba40f7c1a4651
princeitaliya <pditaliya2004@gmail.com>
2023-07-04 10:36:47 +0530

    addd

---
+++ js/jjavascript.js
@@ -0,0 +1,19 @@
+// document.getElementById("demo").innerHTML = "hello hardik";
+
+function mybtn() {
+    alert("hiiii");
+    console.log("hiiiiiiiiiiiiiiiiiiii");
+    document.getElementById("demo").innerHTML = "abcoding 22222";
+}
+
+function color(){
+    alert("hiiii");
+    document.getElementById("color").style.backgroundColor = "red";
+    document.getElementById("color").style.margin = "100px";
+    document.getElementById("color").style.padding = "30px";
+    document.getElementById("color").style.color = "black";
+    document.getElementById("color").style.border = "10px solid black";
+    // document.getElementById("color").style.borderWidth ="5px";
+    // document.getElementById("color").style.borderStyle ="solid";
+    // document.getElementById("color").style.borderWidth ="5px";
+}
---
+++ script.js
@@ -0,0 +1,11 @@
+function abc(){
+    document.getElementById('lorem1').style.color = "red";
+    document.getElementById('lorem1').style.fontSize = "30px";
+    document.getElementById('lorem1').style.backgroundColor = "black";
+}
+document.getElementById('test').innerHTML = "hey Ronit ";
+function myfnd(){
+    // alert("hiiiii");
+    // console.log("hiiii");
+document.getElementById("test1").innerHTML ="hello ronit";
+}
